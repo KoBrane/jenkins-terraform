45cfd6811320e50ec9530929a825d53c55ab7d91
KoBrane <79949603+KoBrane@users.noreply.github.com>
2023-10-14 21:52:17 -0400

    changes

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "this" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
   tags = {
-    name = "terraform-jenkins"
+    Name = "terraform-jenkins"
   }
 }
